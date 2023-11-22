e7d585e61b2d6fe9dc080040f89aec26ff49f671
Darpan Sudan <darpansudan99@gmail.com>
2023-11-22 20:21:01 +0530

    new

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           `${process.env.REACT_APP_SERVER_DOMAIN}/product`,
           {
             method: "GET",
-            //mode: "no-cors",
+            mode: "cors",
           }
         );
         console.log(res);
