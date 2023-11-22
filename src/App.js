67129e5dfeef32c02b070cea7d0f15c9c2dfeac4
Darpan Sudan <darpansudan99@gmail.com>
2023-11-22 19:40:57 +0530

    new

--- src/App.js
+++ src/App.js
@@ -19,9 +19,7 @@ function App() {
             mode: "no-cors",
           }
         );
-        console.log(res);
         const resData = await res.json();
-        console.log(resData);
         dispatch(setDataProduct(resData));
       } catch (error) {
         console.error("Error fetching product data:", error);
