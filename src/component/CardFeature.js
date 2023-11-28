4d4b68d73aee880df0552931fff6973b3e3631a4
Darpan Sudan <darpansudan99@gmail.com>
2023-11-28 20:27:47 +0530

    changed filter

--- src/component/CardFeature.js
+++ src/component/CardFeature.js
@@ -18,7 +18,7 @@ const CardFeature = ({ image, name, price, category, loading, id }) => {
 
   return (
     <div className="w-full min-w-[200px] max-w-[200px] bg-white hover:shadow-lg drop-shadow-lg py-5 px-4 cursor-pointer flex flex-col ">
-      {id ? (
+      {image ? (
         <>
           <Link
             to={`/menu/${id}`}
